a80f21faad22df65747666385ec3dde1a1ca6106
Kkrisi <kadarkrisi@gmail.com>
2024-11-05 15:56:19 +0100

    feltoltes

---
+++ app/Http/Controllers/LendingController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lending;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LendingController extends Controller
+{
+    public function index()
+    {
+        return Lending::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $record = new Lending();
+        $record->fill($request->all());
+        $record->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $user_id, $copy_id, $start)
+    {
+        $lending = Lending::where('user_id', $user_id)
+        ->where('copy_id', $copy_id)
+        ->where('start', $start)
+        //listát ad vissza:
+        ->get();
+        return $lending[0];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $user_id, $copy_id, $start)
+    {
+        $record = $this->show($user_id, $copy_id, $start);
+        $record->fill($request->all());
+        $record->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($user_id, $copy_id, $start)
+    {
+        $this->show($user_id, $copy_id, $start)->delete();
+    }
+
+    public function lendingsWithCopies(){
+        $user = Auth::user();
+        return Lending::with('copies')
+        -> where('user_id', '=', $user->id)
+        -> get();
+    }
+    
+
+    public function dateSpecific(){
+        return Lending::with('specificDate')
+        -> where('start', '=', "1983-06-04")
+        -> get();
+    }
+
+
+    public function copySpecific($copy_id){
+        return Lending::with('copies')
+        -> where('copy_id', '=', $copy_id)
+        -> get();
+    }
+
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\LendingController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//bárki által elérhető
+Route::post('/register',[RegisteredUserController::class, 'store']);
+Route::post('/login',[AuthenticatedSessionController::class, 'store']);
+
+
+
+//összes kérés
+Route::apiResource('/users', UserController::class);
+Route::patch('update-password/{id}', [UserController::class, "updatePassword"]);
+
+//autentikált útvonal
+Route::middleware(['auth:sanctum'])
+    ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::get('lendings-copies', [LendingController::class, "lendingsWithCopies"]);
+        Route::get('user-lendings', [UserController::class, "userLendings"]);
+        // Kijelentkezés útvonal
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    });
+
+//admin
+Route::middleware(['auth:sanctum',Admin::class])
+->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index']);
+    Route::get('specific-date', [LendingController::class, "dateSpecific"]);
+    Route::get('specific-date/{copy_id}', [LendingController::class, "copySpecific"]);
+});
+
+
+
+
+Route::get('books-copies', [BookController::class, "booksWithCopies"]);
+
+
+
+
